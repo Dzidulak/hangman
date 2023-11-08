93aeb9e0d4725519bbe73c86a534d9b3735f9f3e
Dzidulak <dzidulak@gmail.com>
2023-11-08 20:41:36 +0000

    Updated implementation in milestone_4.py and updated README

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 # Hangman
 
-### Project Status
-This project is still in development. **08/11/2023**
-
 # Introduction
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
-## Progress
-So far the program takes in the user input and checks if the letter is in the random word. 
+## Project Status
+This project is still in development. **08/11/2023**
+
+### Progress
+More funtionality has been added. 
+- User enters letter (validity checks are done)
+- If the letter guessed is in the random word the word_guessed variable is updated.
+- If guessed letter is not in the random word the user will lose a life.
 
 # Installation/Usage
 Clone the repo by entering the following in the terminal:
 ```
 git clone https://github.com/Dzidulak/hangman.git
 ```
-Enter the hangman folder and run:
+Enter the hangman folder in the repo and run:
 ```
-milestone_3.py
+milestone_4.py
 ```
+The game can then be played in the terminal.
 
 # File Structure
 ```
@@ -28,6 +32,7 @@ milestone_3.py
 └── hangman
     └── milestone_2.py
     └── milestone_3.py
+    └── milestone_4.py
 ```
 
 # Future Improvements
---
+++ hangman/milestone_4.py
@@ -0,0 +1,58 @@
+import random 
+
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word =  random.choice(word_list) #Random chosen word by the "computer".
+        self.num_letters = len(self.word)
+        self.word_guessed = ['_']*self.num_letters
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def get_duplicate_letters(self, word, letter):
+        start_at = -1
+        indexs = []
+        while True:
+            try:
+                idx = word.index(letter, start_at+1)
+            except ValueError:
+                break
+            else:
+                indexs.append(idx)
+                start_at = idx
+        return indexs
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+
+        if guess in self.word:
+            print(f"Good guess! '{guess}' is in the word.")
+            for letter in self.word:
+                if guess == letter:
+                    indexs = self.get_duplicate_letters(self.word, letter)
+                    for i in indexs:
+                        self.word_guessed[i] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            #print(self.word_guessed)
+            guess = input("Please enter a letter: ")
+
+            if len(guess) != 1 or guess.isalpha() == False:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+    
+
+
+word_list = ["mango","strawberry","papaya","apple","banana"]
+game = Hangman(word_list)
+game.ask_for_input()
